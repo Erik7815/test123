473f14b4bfbeb940acabd08fb07453971eae62a8
Opilane TTHK <opilane@tthk.ee>
2025-10-17 11:22:45 +0300

    switchwithnumbers

---
+++ SwitchWithNumbers/SwitchWithNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SwitchWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            switch(input)
+            {
+                case "1":
+                    Console.Beep();
+                    break;
+                case "2":
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+                case "3":
+                    Console.Beep();
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+
+                    default:
+                    Console.WriteLine("ERROR");
+                    break;
+
+            }
+
+        }
+    }
+}
