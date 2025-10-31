9e58630afca6ec94c0471b03731c8daea80f4018
Opilane TTHK <opilane@tthk.ee>
2025-10-31 11:41:46 +0200

    edjuiec

---
+++ switchNumbers/switchNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace switchNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int nr = Convert.ToInt32(Console.ReadLine());
+            switch (nr)
+            {
+                case 10:
+                    Console.WriteLine("sisestasid numbri 10");
+                    break;
+
+                case 20:
+                    Console.WriteLine("sisestasid numbri 20");
+                    break;
+
+                case 30:
+                    Console.WriteLine("Sisestasid numbri 30");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+
+        }
+    }
+}
