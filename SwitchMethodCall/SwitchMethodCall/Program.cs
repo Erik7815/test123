04241a45c2c9260778f6c50f74b3aa43d8076a4b
Opilane TTHK <opilane@tthk.ee>
2025-10-31 10:53:25 +0200

    ghj

---
+++ SwitchMethodCall/SwitchMethodCall/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SwitchMethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string car = Console.ReadLine();
+
+            switch (car)
+            {
+                case "Coupe":
+                    CarCoupe();
+                    break;
+
+
+                case "Truck":
+                    truck();
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+
+            }
+        }
+
+        static void CarCoupe()
+        {
+            Console.WriteLine("This is a coupe car.");
+        }
+
+        static void truck()
+        {
+            Console.WriteLine("This is a truck");
+        }
+
+        
+
+    }
+}
---
+++ SwitchMultipleCases/SwitchMultipleCases/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SwitchMultipleCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string type;
+
+            type = Console.ReadLine();
+
+            switch (type)
+            {
+                case "Intro to C#":
+                case "Variables":
+                case "Data Types":
+                    Console.WriteLine("Basic");
+                    break;
+
+                case "OOP":
+                case "Classes":
+                case "Objects":
+                    Console.WriteLine("Intermediate");
+                    break;
+
+                case "Asynchronous Programming":
+                case "LINQ":
+                case "delegates":
+                    Console.WriteLine("Advanced");
+                    break;
+
+            }
+
+        }
+    }
+}
