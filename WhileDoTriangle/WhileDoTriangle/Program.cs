15005ea27926f4ecf19130d200e385cfe92b8ae6
Opilane TTHK <opilane@tthk.ee>
2025-11-21 11:06:08 +0200

    kolm

---
+++ WhileDoTriangle/WhileDoTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WhileDoTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kolmnurk");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+
+            while (i <= n)
+            {
+                Console.WriteLine();
+                int j = 1;
+                    while (j <= i)
+                {
+                    Console.WriteLine(j + " ");
+                    j++;
+                }
+
+                i++;
+            }
+            Console.ReadKey(); 
+
+        }
+    }
+}
