3ca05c8d6126b0bc75d95a4a89906becadee2771
Opilane TTHK <opilane@tthk.ee>
2025-11-21 09:36:29 +0200

    atrt

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try Catch Switch");
+
+            try
+            {
+
+
+
+                int nr = int.Parse(Console.ReadLine());
+
+                switch (nr)
+                {
+                    case 1:
+                        Console.WriteLine("Sisestasid numbri 1");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Sisestasid numbri 2");
+                        break;
+
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Palun sisesta number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Sisestatud number on liiga suur või väike!");
+            }
+        }
+    }
+}
