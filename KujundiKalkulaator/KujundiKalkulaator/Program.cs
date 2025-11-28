075b8e01b7006d7312ea26263a4926908444a221
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:02:04 +0200

    htyjj

---
+++ KujundiKalkulaator/KujundiKalkulaator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Channels;
+
+namespace KujundiKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //ruudus sees asub ring. ringi raadius on 3 ühikut.
+            //Leia ja väljesta ekraanile ruudu pindala ja ümbermõõt 
+
+            int radius = 3;
+            int diameter = radius * 2;
+            double squareArea = diameter * diameter;
+            double perimeter = 4 * diameter;
+
+
+            Console.WriteLine("Ruudu pindala on " + squareArea);
+            Console.WriteLine("ruudu ümbermõõt on " + perimeter);
+
+            Console.WriteLine("ringi ümbermõõt on " + Math.PI * 2 * (radius));
+            Console.WriteLine("ringi pindala on " + Math.PI * (radius * radius ));
+
+
+
+        }
+    }
+}
