60ba3f6b10065e26d434003a36911c7930166294
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:45:06 +0200

    frfghy

---
+++ ComputerCalcLogic/ComputerCalcLogic/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ComputerCalcLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kui palju on 3 + 8 / (4 - 2) * 4 = ?");
+
+            double calculation = 3 + 8 / (4 - 2) * 4;
+            float calculation2 = 3 + 8 / 4 - 2 * 4;
+            double calculation3 = (3 + 8) / (4 - 2) * 4;
+            double calculation4 = (3.0 + 8.0) / (4.0 - 2 - 0) * 4.0;
+
+            Console.WriteLine("Vastus: " + calculation);
+            Console.WriteLine("3 + 8 / (4 - 2) * 4 = " + calculation2);
+            Console.WriteLine("(3 + 8) / (4 - 2) * 4 = " + calculation3);
+            Console.WriteLine("(3 + 8) / (4 - 2) * 4 = " + calculation4);
+
+
+        }
+    }
+}
---
+++ MaaKalkulaator/MaaKalkulaator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MaaKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Meil on münt mille diameeter on 25.75 mm
+            //arvuta välja maa ümbermõõt ja mitu münti 
+            //mahub järjestikkus ümber maa 
+            //arvutades tohib ainult kasutada mündi diameetrit ja maa raadiust
+
+            double maaraadius = 637100000;
+            double maaümber = 2 * Math.PI * maaraadius;
+            Console.WriteLine("Maa ümbermõõt on " + maaümber );
+
+            double mündidiameeter = 2.575;
+            Console.WriteLine("Maa ümber mahub " + maaümber  / (mündidiameeter) + " münti");
+
+        }
+    }
+}
