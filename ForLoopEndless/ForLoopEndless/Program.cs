d4eadfa57ba3e7a5df45fcdcc653e19c5e4cae81
Opilane TTHK <opilane@tthk.ee>
2025-12-05 10:00:10 +0200

    frlpend

---
+++ ForLoopEndless/ForLoopEndless/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ForLoopEndless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ushort i = 0;
+
+            //kui 10 rida konsoolis sis lõpetab ära
+
+            for (; ; )
+            {
+                Console.WriteLine("i väärtus on " + i);
+                i++;
+
+                if (i == 10)
+                    break;
+            }
+        }
+    }
+}
