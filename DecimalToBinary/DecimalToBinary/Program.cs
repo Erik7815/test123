f570cef6b1ee17fa536c05777489b20b69d02b6e
Opilane TTHK <opilane@tthk.ee>
2025-12-15 09:58:20 +0200

    kkstnd

---
+++ DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DecimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decimal to binary");
+
+            Console.WriteLine("sisesta number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string binarynumber = "";
+
+            while (num > 0)
+            {
+                int reminder = num % 2;
+                binarynumber = reminder + binarynumber;
+                num /= 2;
+            }
+            Console.WriteLine(binarynumber);
+        }
+    }
+}
---
+++ FibonacciFor/FibonacciFor/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FibonacciFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\n");
+            Console.WriteLine("Näita esimest Fibonacci seeriat");
+            Console.WriteLine("-------------------------------\n\n");
+            Console.Write("Sisesta Number ");
+            int prv = 0, pre = 1, trm, i, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Siin on Fibonacci seeria {0} kuni \n", n);
+
+            Console.WriteLine($"{0} {1}", prv, pre);
+            trm = prv + pre;
+
+            for (i = 3; i <= n; i++)
+            {
+                Console.Write(trm + " ");
+                prv = pre;
+                pre = trm;
+
+
+            }
+            Console.Write("\n");
+        }
+    }
+}
