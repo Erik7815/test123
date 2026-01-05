188ffddec396d89cdce6d444b09f283f6539a1c6
Opilane TTHK <opilane@tthk.ee>
2026-01-05 09:52:37 +0200

    l

---
+++ AnimalLength/AnimalLength/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AnimalLength
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string[] loom = { "koer", "kass", "jänes", "sipelgas", "lendorav" };
+
+            int longername = loom[0].Length;
+            string longernamestring = loom[0];
+
+            for (int i = 0; i < loom.Length; i++)
+            {
+                if (longername < loom[i].Length)
+                {
+                    longername = loom[i].Length;
+                    longernamestring = loom[i];
+                }
+                int animalength = animallength(loom[i]);
+                Console.WriteLine(loom[i] + " = ");
+                Console.WriteLine(animalength);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("pikim nimi oli {0} ja mille nime pikkus oli: {1}", longernamestring, longername);
+        }
+        static int animallength(string loom)
+        {
+            return loom.Length;
+        }
+    }
+}
---
+++ ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //vaja teha string array H, e, l, l, o
+            //kasuta for loopi
+
+            string[] hello = { "H", "e", "l", "l", "o" };
+
+            for (int i = 0; i < hello.Length; i++)
+            {
+                Console.WriteLine(hello[i]);
+            }
+
+            //kasuta nüüd foreach-i
+            foreach (string i  in hello)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
