8e521bcf6559be2e9aa3e0f09f93d21abc37c672
Opilane TTHK <opilane@tthk.ee>
2026-01-16 11:41:13 +0200

    lu

---
+++ FileWordReader/FileWordReader/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace FileWordReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Luuletus");
+        }
+
+        static void filewordreader()
+        {
+
+            string line;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/opilane/Desktop/luuletus.txt"))
+                {
+                    int cnt = 0;
+                    while (sr.EndOfStream == false)
+                    {
+                        ++cnt;
+                        line = sr.ReadLine();
+                        int pikkus = line.Length;
+                        Console.WriteLine(cnt + " " + line + " " + pikkus);
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("faili ei saa lugeda");
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
